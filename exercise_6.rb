ab36ad522df2ba9840aba22efd8ec4d255b0762c
Marianne OConnor <marianneoco@gmail.com>
2019-10-21 15:26:08 +0100

    Answer to exercise 5 and starting exercise 6

--- exercise_6.rb
+++ exercise_6.rb
@@ -13,3 +13,34 @@ assert_equal(
   pig_latin_sentence_reverse('Ethay ickquay oxfay umpedjay overway ethay azylay ogday'),
   'The quick fox jumped over the lazy dog'
 )
+
+def pig_latin_sentence(sentence)
+  pig_latin_array = []
+  sentence.split(" ").each do |word|
+    pig_latin_array.append(pig_latin(word))
+  end
+  return pig_latin_array.join(" ")
+end
+
+def preserve_capitals(original_word, final_word)
+  if original_word[0] == original_word[0].upcase
+    return final_word.capitalize
+  else
+    return final_word
+  end
+   # will return word with same capitalisation as orignal
+end
+
+def pig_latin(word)
+    if "aeiou".include?(word[0])
+      return preserve_capitals(word, word + "way")
+    elsif "qu" == word[0, 2]
+      return preserve_capitals(word, word[2..-1] + "quay")
+    else
+      word.each_char.with_index(0) do |c, i|
+        if "aeiou".include?(c)
+          return preserve_capitals(word, word[i..-1] + word[0...i] + "ay")
+      end
+    end
+  end
+end
